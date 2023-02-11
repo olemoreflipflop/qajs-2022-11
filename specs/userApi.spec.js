38afb3454d8b02b9366c2ef77ff765ba2a75140d
olemoreflipflop <fil.olga.ev@gmail.com>
2023-02-11 22:41:07 +0300

    1st part of refactoring - faker, http methods isolation + before/after

--- specs/userApi.spec.js
+++ specs/userApi.spec.js
@@ -1,128 +1,169 @@
 import config from "./framework/config";
 import account from "./framework/services/user";
+import { createUser } from "./framework/utils/generators";
 
-describe("bookstore user API tests", () => {
+describe("User API tests", () => {
   const { credentials } = config;
-  let user = {};
-  let token = '';
 
-  it("POST /account/v1/user - Create user, valid password(201)", async () => {
-    const res = await account.user(credentials);
-    expect(res.status).toEqual(201);
-    user = res.body;
-    token = await account.getAuthToken(credentials);
-    console.log(user)
-  });
+  describe("POST /account/v1/user", () => {
+    let userData;
+    const validUser = createUser().getData();
+    const userShortPassword = createUser().generateShortPassword().getData();
+    const userInvalidPassword = createUser().generateShortPassword().getData();
+    console.log(validUser, userShortPassword, userInvalidPassword)
 
-  it("POST /account/v1/user - Create user, User exists(406)", async () => {
-    const body = {
-      "code": "1204",
-      "message": "User exists!"
-    };
-    const res = await account.user(credentials);
-    expect(res.status).toEqual(406);
-    expect(res.body).toEqual(body);
-  });
-
-  it("POST /account/v1/user - Create user, Invalid password(400)", async () => {
-    const body = {
-      code: "1300",
-      message: "Passwords must have at least one non alphanumeric character, one digit ('0'-'9'), one uppercase ('A'-'Z'), one lowercase ('a'-'z'), one special character and Password must be eight characters or longer."
-    }
-
-    const res = await account.user({
-      userName: 'demo_OL12',
-      password: '123456'
+    afterAll(async () => {
+      await account.deleteUser(userData.uuid, userData.token);
     });
 
-    expect(res.status).toEqual(400);
-    expect(res.body).toEqual(body);
-  });
+    it("Creates user, valid password(201)", async () => {
+      const res = await account.user(validUser);
+      const authToken = await account.getAuthToken(validUser);
+      userData = { uuid: res.body.userID, token: authToken };
 
-  it("POST /account/v1/generatetoken - New token, User exists(200)", async () => {
-    const payload = {
-      userName: 'tokenUser',
-      password: '123456Qaz!'
-    }
-    const userData = await account.user(payload);
-    const res = await account.generateToken(payload);
-    await account.deleteUser(userData.body.userID, res.body.token);
-
-    expect(res.status).toEqual(200);
-    expect(res.body.token).not.toBe(null);
-    expect(typeof res.body.token).toEqual('string')
-  });
-
-  it("POST /account/v1/generatetoken - New token, User doesn't exists(200)", async () => {
-    const res = await account.generateToken({
-      userName: 'test_001',
-      password: '123456'
+      expect(res.status).toEqual(201);
     });
 
-    expect(res.status).toEqual(200);
-    expect(res.body.token).toBe(null);
-  });
+    it("Doesn't create user, User exists(406)", async () => {
+      const body = {
+        "code": "1204",
+        "message": "User exists!"
+      };
+      const res = await account.user(validUser);
 
-  it("POST /account/v1/authorized - Auth user, valid credentials(200)", async () => {
-    const res = await account.authorized(credentials);
-
-    expect(res.status).toEqual(200);
-    expect(res.body).toEqual(true);
-  });
-
-  it(`GET /account/v1/user/{uuid} - Get user info, valid credentials(200)`, async () => {
-    const res = await account.userInfo(user.userID, token);
-    console.log(user.userID, token)
-    expect(res.status).toEqual(200);
-    expect(res.body.username).toEqual(user.username);
-  });
-
-  it(`GET /account/v1/user/{uuid}- Get user info, invalid userID(401)`, async () => {
-    const res = await account.userInfo("123", token);
-    expect(res.status).toEqual(401);
-    expect(res.body).toEqual({ code: "1207", message: "User not found!" });
-  });
-
-  it(`GET /account/v1/user/{uuid}- Get user info, invalid token(401)`, async () => {
-    const res = await account.userInfo(user.userID, "");
-    expect(res.status).toEqual(401);
-    expect(res.body).toEqual({ code: "1200", message: "User not authorized!" });
-  });
-
-  it(`DELETE /account/v1/user/{uuid} - Delete user,  invalid token(401)`, async () => {
-    const res = await account.deleteUser(user.userID, "");
-
-    expect(res.status).toEqual(401);
-    expect(res.body).toEqual({ code: "1200", message: "User not authorized!" });
-  });
-
-  it(`DELETE /account/v1/user/{uuid} - Delete user,  invalid userID(200)`, async () => {
-    const res = await account.deleteUser("123", token);
-
-    expect(res.status).toEqual(200);
-    expect(res.body).toEqual({ code: "1207", message: "User Id not correct!" });
-  });
-
-  it(`DELETE /account/v1/user/{uuid} - Delete user, valid credentials(200)`, async () => {
-    const res = await account.deleteUser(user.userID, token);
-    expect(res.status).toEqual(204);
-  });
-
-  it("POST /account/v1/authorized - Auth user, user doesn't exists(404)", async () => {
-    const res = await account.authorized({
-      userName: 'demo_OL12',
-      password: '123456'
+      expect(res.status).toEqual(406);
+      expect(res.body).toEqual(body);
     });
 
-    expect(res.status).toEqual(404);
-    expect(res.body).toEqual({ "code": "1207", "message": "User not found!" });
+    it("Doesn't create user, Invalid password(400)", async () => {
+      const body = {
+        code: "1300",
+        message: "Passwords must have at least one non alphanumeric character, one digit ('0'-'9'), one uppercase ('A'-'Z'), one lowercase ('a'-'z'), one special character and Password must be eight characters or longer."
+      }
+      const res = await account.user(userInvalidPassword);
+
+      expect(res.status).toEqual(400);
+      expect(res.body).toEqual(body);
+    });
+
+    it("Doesn't create user, Short password(400)", async () => {
+      const body = {
+        code: "1300",
+        message: "Passwords must have at least one non alphanumeric character, one digit ('0'-'9'), one uppercase ('A'-'Z'), one lowercase ('a'-'z'), one special character and Password must be eight characters or longer."
+      }
+      const res = await account.user(userShortPassword);
+
+      expect(res.status).toEqual(400);
+      expect(res.body).toEqual(body);
+    });
+
   });
 
-  it("POST /account/v1/authorized - Auth user, empty payload(400)", async () => {
-    const res = await account.authorized();
+  describe("DELETE /account/v1/user/{uuid}", () => {
+    const user = createUser().getData();
+    let userData;
 
-    expect(res.status).toEqual(400);
-    expect(res.body).toEqual({ code: '1200', message: 'UserName and Password required.' });
+    beforeAll(async () => {
+      userData = await account.createUserWithToken(user);
+    });
+
+    //нужно ли здесь удаление после всех тестов? смущает оставление юзера в базе, если тест на удаление упадет
+
+    it("Doesn't delete user with invalid token(401)", async () => {
+      const res = await account.deleteUser(userData.uuid, "");
+      console.log(res.body)
+      expect(res.status).toEqual(401);
+      expect(res.body).toEqual({ code: "1200", message: "User not authorized!" });
+    });
+
+    it("Doesn't delete user with invalid user UUID(200)", async () => {
+      const res = await account.deleteUser("123", userData.token);
+
+      expect(res.status).toEqual(200);
+      expect(res.body).toEqual({ code: "1207", message: "User Id not correct!" });
+    });
+
+    it("Deletes user with valid UUID & token(200)", async () => {
+      const res = await account.deleteUser(userData.uuid, userData.token);
+
+      expect(res.status).toEqual(204);
+    });
+  })
+
+  describe("POST /account/v1/authorized", () => {
+    it("Doesn't auth user if user doesn't exists(404)", async () => {
+      const res = await account.authorized(createUser().getData());
+
+      expect(res.status).toEqual(404);
+      expect(res.body).toEqual({ "code": "1207", "message": "User not found!" });
+    });
+
+    it("Doesn't auth user, empty payload(400)", async () => {
+      const res = await account.authorized();
+
+      expect(res.status).toEqual(400);
+      expect(res.body).toEqual({ code: '1200', message: 'UserName and Password required.' });
+    });
+
+    it("Authorizes user with valid credentials(200)", async () => {
+      // console.log(credentials.toString('ascii'));
+      const res = await account.authorized(credentials);
+      console.log(res.body);
+
+      expect(res.status).toEqual(200);
+      expect(res.body).toEqual(true);
+    });
+
+  });
+
+  describe("POST /account/v1/generatetoken", () => {
+    it("Generates new token, User exists(200)", async () => {
+      const res = await account.generateToken(credentials);
+
+      expect(res.status).toEqual(200);
+      expect(res.body.token).not.toBe(null);
+      expect(typeof res.body.token).toEqual('string')
+    });
+
+    it("Doesn't generates token if user doesn't exists(200)", async () => {
+      const res = await account.generateToken(createUser().getData());
+
+      expect(res.status).toEqual(200);
+      expect(res.body.token).toBe(null);
+    });
+
+  });
+
+  describe("GET /account/v1/user/{ uuid }", () => {
+    const user = createUser().getData();
+    let userData;
+
+    beforeAll(async () => {
+      userData = await account.createUserWithToken(user);
+    });
+
+    afterAll(async () => {
+      await account.deleteUser(userData.uuid, userData.token);
+    });
+
+    it("Gets user info if credentials are valid(200)", async () => {
+      const res = await account.userInfo(userData.uuid, userData.token);
+      console.log(res.body)
+      expect(res.status).toEqual(200);
+      expect(res.body.username).toEqual(user.userName);
+    });
+
+    it("Doesn't get user info if used invalid UUID(401)", async () => {
+      const res = await account.userInfo("123", userData.token);
+      expect(res.status).toEqual(401);
+      expect(res.body).toEqual({ code: "1207", message: "User not found!" });
+    });
+
+    it("Doesn't get user info if used invalid token(401)", async () => {
+      const res = await account.userInfo(userData.uuid, "");
+      expect(res.status).toEqual(401);
+      expect(res.body).toEqual({ code: "1200", message: "User not authorized!" });
+    });
   });
 
 });
